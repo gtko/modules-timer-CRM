26cf905ab04fde5b12ce3297bd5364982689d460
gtko <gtko@users.noreply.github.com>
2022-04-08 12:17:36 +0200

    last fix

--- Http/Livewire/TimeTrackerCommercial.php
+++ Http/Livewire/TimeTrackerCommercial.php
@@ -24,8 +24,6 @@ class TimeTrackerCommercial extends Component
     {
         $this->countTimer = $repTimer->fetchTimerStartedInSinceWhenTime(\Auth::commercial());
 
-
-
         $timer = $repTimer->fetchTimerStarted(\Auth::commercial());
 
         if ($timer) {
